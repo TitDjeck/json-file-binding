cfdca6f22f693e8e25a36933d55a55d3243cda39
Djeck <bertonmarc@gmail.com>
2015-09-10 09:28:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+﻿var util = require("util"),
+    fs = require("fs"),
+    extend = require("extend");
+
+module.exports = function (filePath, data, autoSave) {
+        
+    if (fs.existsSync(filePath)) {
+        extend(true, data, JSON.parse(fs.readFileSync(filePath, 'utf8')));
+    }
+    
+    if (autoSave && (util.isArray(data) || util.isObject(data))) {
+        Object.observe(data, function (changes) {
+            fs.writeFile(filePath, JSON.stringify(data), 'utf8', function (err) {
+                if (err) console.log(util.format("json-file-binding autoSave error : %j", err));
+            });
+        });
+    }
+    
+    process.on('SIGINT', function () {
+        process.exit();
+    });
+
+    process.on("exit", function () {
+        fs.writeFileSync(filePath, JSON.stringify(data), 'utf8');    
+    });
+};
