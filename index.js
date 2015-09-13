7f33d582bd7bd1bb20fefe1bf6da031d3554b174
Djeck <bertonmarc@gmail.com>
2015-09-13 14:27:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-﻿var util = require("util"),
+var util = require("util"),
     fs = require("fs"),
-    extend = require("extend");
+    extend = require("extend"),
+    JSONS = require("json-serialize");
 
 module.exports = function (filePath, data, autoSave) {
-        
+    
     if (fs.existsSync(filePath)) {
-        extend(true, data, JSON.parse(fs.readFileSync(filePath, 'utf8')));
+        extend(true, data, JSONS.deserialize(JSON.parse(fs.readFileSync(filePath, 'utf8'))));
     }
     
     if (autoSave && (util.isArray(data) || util.isObject(data))) {
         Object.observe(data, function (changes) {
-            fs.writeFile(filePath, JSON.stringify(data), 'utf8', function (err) {
+            fs.writeFile(filePath, JSON.stringify(JSONS.serialize(data)), 'utf8', function (err) {
                 if (err) console.log(util.format("json-file-binding autoSave error : %j", err));
             });
         });
@@ -19,8 +20,8 @@ module.exports = function (filePath, data, autoSave) {
     process.on('SIGINT', function () {
         process.exit();
     });
-
+    
     process.on("exit", function () {
-        fs.writeFileSync(filePath, JSON.stringify(data), 'utf8');    
+        fs.writeFileSync(filePath, JSON.stringify(JSONS.serialize(data)), 'utf8');
     });
 };
